7307b9e8aa0cfc3e40f88b9336fbd261663d8879
unknown <wijayak2508@gmail.com>
2025-07-05 12:16:11 +0700

    new sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://baliplay.vercel.app/', // ganti dengan domain kamu
+  generateRobotsTxt: true, // (optional)
+};
